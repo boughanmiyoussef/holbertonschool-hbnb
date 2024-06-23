63156fe085832d378f0afc53893fecd0e2e671e9
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-06-23 23:01:39 +0100

    User CRUD Is Done

--- app.py
+++ app.py
@@ -21,7 +21,30 @@ def manage_users():
         data_manager.save('users', data['id'], data)
         return make_response(jsonify(data), 201)
 
+@app.route('/users/<user_id>', methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
+def manage_user(user_id):
+    user = data_manager.get('users', user_id)
+    if not user:
+        return make_response(jsonify({'error': 'User not found'}), 404)
 
+    if request.method == 'GET':
+        return jsonify(user)
+
+    if request.method == 'PUT':
+        data = request.get_json()
+        if not data:
+            return make_response(jsonify({'error': 'No data provided'}), 400)
+        data['id'] = user_id
+        data['updated_at'] = datetime.now().isoformat()
+        data_manager.update('users', user_id, data)
+        return jsonify(data)
+
+    if request.method == 'DELETE':
+        data_manager.delete('users', user_id)
+        return make_response('User Successfully Deleted', 204)
+
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
